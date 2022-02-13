472e2bdf2a97e3bd19d3937ce4be10c23ee4940a
Christopher Read <Christopherread22@outlook.com>
2022-02-13 16:32:42 -0500

    controller setup

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,44 @@
 class ProductsController < ApplicationController
+
+    def index
+        @products = Product.order(:id)
+        render json: @products
+    end
+
+    def show
+        @product = Product.find(params[:id])
+        render json: @product
+    end
+
+    def new
+        @product = Product.new
+        render json: @product
+    end
+
+    def create
+        @product = Product.create(product_params)
+        render json: @product
+    end
+
+    def edit
+        @product = Product.find(params[:id])
+        render json: @product
+    end
+
+    def update
+        @product = product.find(params[:id])
+        @product.update(product_params)
+        render json: @product
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+        render json: @product
+    end
+
+    def product_params
+        params.permit(:name, :price, :description)
+    end
+    
 end
