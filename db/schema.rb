a0a0169d1a62f018aa6aed675ad118ce62c47492
Christopher Read <Christopherread22@outlook.com>
2022-02-14 16:45:04 -0500

    Finished

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2022_02_13_194035) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
-    t.integer "description"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,20 @@
-products = [
-    {
-        name: "Cash register",
-        price: 800,
-        description: "Front house cash register"
-    },
-    {
-        name: "Line Grill",
-        price: 1000,
-        description: "Main grill for line cooks"
-    },
-    {
-        name: "Kitchen Utensils",
-        price: 200,
-        description: "Knives, spatula's etc for line cooks"
-    },
-    {
-        name: "Freezers",
-        price: 1000,
-        description: "Freezers for produce"
-    }
-]
+
+   Product1 = Product.create(
+       name: "Cash Register",
+       price: 500,
+       description: "Front house cash register"
+   )
+
+   Product2 = Product.create(
+       name: "Line Grill",
+       price: 1000,
+       description: "Main grill for line cooks"
+   )
+
+   Product3 = Product.create(
+    name: "Window Promotion",
+    price: 100,
+    description: "Latest promotion to hang in front window"
+)
 
 puts "Products added"
